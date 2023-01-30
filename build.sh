3da1c4aa42fca1cdc04dce1f045f0a5ce9e74b4e
Evan Overman <evanrileyoverman@gmail.com>
2023-01-30 12:15:30 -0800

    Wall and Wextra

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 mkdir -p build/
-gcc -c scrutiny/scrutiny.c -o build/scrutiny.o
+gcc -Wall -Wextra -c scrutiny/scrutiny.c -o build/scrutiny.o
 ar rcs build/scrutiny.a build/scrutiny.o
 
